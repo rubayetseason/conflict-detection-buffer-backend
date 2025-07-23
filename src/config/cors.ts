a71a8fd9a2942426de921e634023fb540fb0c0a5
rubayetseason <rubayetislamseason5@gmail.com>
2025-07-23 18:07:39 +0600

    [FIX] --> newcors url added

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,6 +3,7 @@ const corsConfig = {
     'http://localhost:5173',
     'http://localhost:3000',
     'https://conflict-detection-buffer-frontend.vercel.app',
+    'https://conflict-detection-buffer-frontend.vercel.app/',
   ],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
